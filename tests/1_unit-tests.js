716fe61c72bb6eecbc1c7a2c2152e01895c3fe2f
DanHexaForge <danielmusyoki48@gmail.com>
2025-07-09 21:48:48 +0300

    first test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,10 +4,11 @@ const assert = chai.assert;
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
-    test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+    test('isNull, isNotNull', function () {
+      assert.isNull(null, 'This is an optional error description - e.g. null is null');
+      assert.isNotNull(1, '1 is not null');
     });
+ });
     // #2
     test('#isDefined, #isUndefined', function () {
       assert.fail(null, 'null is not undefined');
@@ -120,7 +121,7 @@ suite('Unit Tests', function () {
       assert.fail(formatPeople('John Doe', 35), regex);
       assert.fail(formatPeople('Paul Smith III', 'twenty-four'), regex);
     });
-  });
+  
 
   // -----------------------------------------------------------------------------
 
